535ed40bc2c6cbeb0e8fcc2178981e7583936062
Cigomba Miler <cmiler@asu.edu>
2024-02-17 11:16:20 -0700

    This branch is lit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { marked } from "https://cdn.jsdelivr.net/npm/marked/lib/marked.esm.js";
+import React, { useState, useEffect, useRef } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const editor = useRef(null);
+	const preview = useRef(null);
+
+	const [markdown, setMarkdown] = useState("# Enter markdown here");
+
+	useEffect(() => {
+		// renders every time the component is updated
+		const previewRef = preview.current;
+		previewRef.innerHTML = marked.parse(markdown);
+	}, [markdown]);
+
+	return (
+		<div className="App">
+			<h1>MarkDown Previewer</h1>
+			<section id="editor-section">
+				<textarea
+					id="editor"
+					ref={editor}
+					rows="10"
+					cols="50"
+					value={markdown}
+					onChange={(e) => setMarkdown(e.target.value)}></textarea>
+			</section>
+			<section id="markdown-section">
+				<h1 style={{ textAlign: "left" }}>Preview</h1>
+				<div
+					id="preview"
+					ref={preview}></div>
+			</section>
+		</div>
+	);
 }
 
 export default App;
